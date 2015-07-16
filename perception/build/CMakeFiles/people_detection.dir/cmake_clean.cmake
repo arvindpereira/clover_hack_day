51d6538201d9eed04bd06ce26250e179ea8ee912
Arvind Pereira <arvind.pereira@gmail.com>
2015-07-16 07:27:10 -0700

    Perception portion based on PCL that was later moved into a ROS node by itself.

---
+++ perception/build/CMakeFiles/people_detection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/people_detection.dir/ground_based_people_detection.cpp.o"
+  "people_detection.pdb"
+  "people_detection"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/people_detection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
